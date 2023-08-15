a611d16ebb04ab10a6c5ab8ca2a4bdb4532049f5
Avaer Kazmer <a@modules.io>
2023-08-15 00:08:46 -0400

    Fix tests

--- agents.js
+++ agents.js
@@ -1,10 +1,12 @@
+import dotenv from 'dotenv';
 import {createClient} from '@supabase/supabase-js';
 
-export const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || "https://friddlbqibjnxjoxeocc.supabase.co";
-export const SUPABASE_PUBLIC_API_KEY = import.meta.env.VITE_SUPABASE_PUBLIC_API_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZyaWRkbGJxaWJqbnhqb3hlb2NjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODkwNjMxMjcsImV4cCI6MjAwNDYzOTEyN30.cP4pmsmvztz-In-xHxVGXpjU-2vEnNKxxKiGV2R7irw";
+dotenv.config();
+const SUPABASE_URL = process.env.VITE_SUPABASE_URL || "https://friddlbqibjnxjoxeocc.supabase.co";
+const SUPABASE_PUBLIC_API_KEY = process.env.VITE_SUPABASE_PUBLIC_API_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZyaWRkbGJxaWJqbnhqb3hlb2NjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODkwNjMxMjcsImV4cCI6MjAwNDYzOTEyN30.cP4pmsmvztz-In-xHxVGXpjU-2vEnNKxxKiGV2R7irw";
 
 class Agents {
-  static connect() {
+  static async connect() {
     const clientA = createClient(SUPABASE_URL, SUPABASE_PUBLIC_API_KEY);
     const channelA = clientA.channel('room-1');
     channelA
