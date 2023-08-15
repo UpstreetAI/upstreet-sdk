d0452813b6df061ca90c1cd73eed690a9e20a31c
Avaer Kazmer <a@modules.io>
2023-08-15 01:08:56 -0400

    Update CLI test

--- clients/agents.js
+++ clients/agents.js
@@ -10,19 +10,51 @@ class Agents {
         persistSession: false,
       },
     });
+    // client gets bastardized inheritance of the methods from EventEmitter
+    // https://nodejs.org/api/events.html#events_class_eventemitter
+
     const c = client.channel('room-1');
     c
       .on(
         'broadcast',
         {
-          event: 'test',
+          event: 'chat',
         },
-        (payload) => {
-          console.log('got payload', payload);
+        (o) => {
+            c2.dispatchEvent(new MessageEvent('chat', {
+                data: o.payload.message,
+            }));
         },
       )
-      .subscribe();
-    return c;
+    c
+      .on(
+        'broadcast',
+        {
+          event: 'json',
+        },
+        (o) => {
+            let result, error;
+            try {
+                result = JSON.parse(o.payload.message);
+            } catch(err) {
+                error = err;
+            }
+            if (!error) {
+                c2.dispatchEvent(new MessageEvent('json', {
+                    data: result,
+                }));
+            } else {
+                console.warn(error);
+            }
+        },
+      )
+    c.subscribe();
+
+    const c2 = new EventTarget();
+    c2.send = function() {
+        return c.send.apply(c, arguments);
+    }
+    return c2;
   }
 }
 export const agents = Agents;
--- test.js
+++ test.js
@@ -1,22 +1,53 @@
+import repl from 'repl';
 import {agents} from './index.js';
 
 const test = async () => {
-  const c1 = await agents.connect();
-  c1.send({
-    type: 'broadcast',
-    event: 'test',
-    payload: {
-      message: 'hello, world 1'
-    },
-  });
+  const customEval = (cmd, context, filename, callback) => {
+    cmd = cmd.replace(/\n+$/, '');
+    const match = cmd.match(/^\/(\S+)\s*([\s\S]*)$/);
+    // console.log('match', [cmd, match]);
+    if (match) {
+      const [_, command, commandArgumentsString] = match;
+      const commandArguments = commandArgumentsString.split(/\s+/);
 
-  const c2 = await agents.connect();
-  c2.send({
-    type: 'broadcast',
-    event: 'test',
-    payload: {
-      message: 'hello, world 2'
-    },
+      connection.send({
+        type: 'broadcast',
+        event: 'json',
+        payload: {
+          message: JSON.stringify({
+            command,
+            commandArguments,
+          }, null, 2),
+        },
+      });
+    } else {
+      connection.send({
+        type: 'broadcast',
+        event: 'chat',
+        payload: {
+          message: cmd,
+        },
+      });
+    }
+    // callback(null, '');
+  };
+
+  const connection = await agents.connect();
+
+  // Run a repl with custom evaluation
+  const r = repl.start({
+    // prompt: '> ',
+    prompt: '',
+    eval: customEval
+  });
+  r.context.connection = connection;
+
+  connection.addEventListener('chat', (e) => {
+    console.log(e.data);
+  });
+  connection.addEventListener('json', (e) => {
+    console.log(e.data);
   });
 };
+
 test();
